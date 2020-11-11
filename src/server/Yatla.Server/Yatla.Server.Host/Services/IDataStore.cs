9012c37deff69efa13b0bce08e4cedb689c75de8
Luke Parker <lparkermg@outlook.com>
2020-11-11 21:28:14 +0000

    Adds JsonDataStoreService.

---
+++ src/server/Yatla.Server/Yatla.Server.Host/Services/IDataStore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Yatla.Server.Host.Services
+{
+    interface IDataStore<T>
+    {
+        Task<IList<T>> Get(int skip, int take);
+
+        Task Save(T data);
+    }
+}
