425c7413d39ffccdd56fa7c4c0079d4beb27842c
Luke Parker <lparkermg@outlook.com>
2020-11-13 23:01:20 +0000

    Added env variables for docker support

--- src/server/Yatla.Server/Yatla.Server.Host/Program.cs
+++ src/server/Yatla.Server/Yatla.Server.Host/Program.cs
@@ -18,9 +18,7 @@ namespace Yatla.Server
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .ConfigureAppConfiguration(builder => builder.AddEnvironmentVariables("YATLA_"));
     }
 }
