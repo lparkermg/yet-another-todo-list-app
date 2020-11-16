072affce155d9f7031b83ff6f664f25496752c96
Luke Parker <lparkermg@outlook.com>
2020-11-16 13:04:38 +0000

    Moved click handling from tr to tds in item list. (may move to button)

--- src/ui/yatla-ui/src/Components/TodoItem.jsx
+++ src/ui/yatla-ui/src/Components/TodoItem.jsx
@@ -14,10 +14,10 @@ function TodoItem(props) {
 
     return (
     <>
-        <tr className="TodoItem-Row" onClick={() => handleMarkAsDone(props.itemId)} hidden={isHidden}>
-            <td>{new Date(props.createdAt).toLocaleString('en-gb')}</td>
-            <td className="TodoList-Table__Cell_Large">{props.data}</td>
-                <td><button onClick={() => setEditing(!isEditing)}>Edit</button></td>
+        <tr className="TodoItem-Row" hidden={isHidden}>
+            <td onClick={() => handleMarkAsDone(props.itemId)}>{new Date(props.createdAt).toLocaleString('en-gb')}</td>
+            <td onClick={() => handleMarkAsDone(props.itemId)} className="TodoList-Table__Cell_Large">{props.data}</td>
+            <td><button onClick={() => setEditing(!isEditing)}>Edit</button></td>
         </tr>
             {isEditing ?? (
                 <tr className="TodoItem-Row" hidden={isHidden}>
